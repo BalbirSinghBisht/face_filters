48bb1bae513b8cf45c1ff9aa0330c709438a95e6
balbir singh bisht <balbirbisht560@gmail.com>
2021-10-02 10:57:00 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -14,20 +14,27 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  late CameraDeepArController cameraDeepArController;
-  int count = 0;
+  CameraDeepArController? cameraDeepArController;
+  int currentPage = 0;
+  final vp = PageController(viewportFraction: .24);
+  Effects currentEffect = Effects.none;
+  Filters currentFilter = Filters.none;
+  Masks currentMask = Masks.none;
+  bool isRecording = false;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Face-Filters',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: Stack(
-          children: <Widget>[
+          children: [
             CameraDeepAr(
                 onCameraReady: (isReady) {
                   setState(() {});
@@ -36,6 +43,7 @@ class _MyAppState extends State<MyApp> {
                   setState(() {});
                 },
                 onVideoRecorded: (path) {
+                  isRecording = false;
                   setState(() {});
                 },
                 androidLicenceKey: apiKey,
@@ -48,15 +56,94 @@ class _MyAppState extends State<MyApp> {
               alignment: Alignment.bottomCenter,
               child: Container(
                 padding: EdgeInsets.all(20),
-                child: FloatingActionButton(
-                    onPressed: () {
-                      cameraDeepArController.changeMask(count);
-                      count == 7 ? count = 0 : count++;
-                    },
-                  child: Icon(Icons.navigate_next,color: Colors.yellow,),
+                //height: 250,
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextButton(
+                            onPressed: () {
+                              if (null == cameraDeepArController) return;
+                              if (isRecording) return;
+                              cameraDeepArController!.snapPhoto();
+                            },
+                            child: Icon(Icons.camera_enhance_outlined),
+                            style: TextButton.styleFrom(
+                              backgroundColor: Colors.white,
+                              padding: EdgeInsets.all(15),),
+                          ),
+                        ),
+                        if (isRecording)
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {
+                                if (null == cameraDeepArController) return;
+                                cameraDeepArController!.stopVideoRecording();
+                                isRecording = false;
+                                setState(() {});
+                              },
+                              child: Icon(Icons.videocam_off),
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.red,
+                                padding: EdgeInsets.all(15),),
+                            ),
+                          )
+                        else
+                          Expanded(
+                            child: TextButton(
+                              onPressed: () {
+                                if (null == cameraDeepArController) return;
+                                cameraDeepArController!.startVideoRecording();
+                                isRecording = true;
+                                setState(() {});
+                              },
+                              child: Icon(Icons.videocam),
+                              style: TextButton.styleFrom(
+                                backgroundColor: Colors.green,
+                                padding: EdgeInsets.all(15),),
+                            ),
+                          ),
+                      ],
+                    ),
+                    SingleChildScrollView(
+                      padding: EdgeInsets.all(15),
+                      scrollDirection: Axis.horizontal,
+                      child: Row(
+                        children: List.generate(Masks.values.length, (p) {
+                          bool active = currentPage == p;
+                          return GestureDetector(
+                            onTap: () {
+                              currentPage = p;
+                              cameraDeepArController!.changeMask(p);
+                              setState(() {});
+                            },
+                            child: Container(
+                                margin: EdgeInsets.all(5),
+                                width: active ? 40 : 30,
+                                height: active ? 50 : 40,
+                                alignment: Alignment.center,
+                                decoration: BoxDecoration(
+                                    color:
+                                    active ? Colors.orange : Colors.white,
+                                    shape: BoxShape.circle),
+                                child: Text(
+                                  "$p",
+                                  textAlign: TextAlign.center,
+                                  style: TextStyle(
+                                      fontSize: active ? 16 : 14,
+                                      color: Colors.black),
+                                )),
+                          );
+                        }),
+                      ),
+                    )
+                  ],
                 ),
               ),
-            ),
+            )
           ],
         ),
       ),
